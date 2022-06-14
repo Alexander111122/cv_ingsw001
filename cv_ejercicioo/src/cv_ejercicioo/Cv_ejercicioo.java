c3c701c2fb249ae30c0ef195c9b1bb8340140b84
Alexander <Alexander@DESKTOP-EP12OEF>
2022-06-14 17:51:54 -0500

    comentario 11

---
+++ cv_ejercicioo/src/cv_ejercicioo/Cv_ejercicioo.java
@@ -0,0 +1,20 @@
+
+package cv_ejercicioo;
+
+/**
+ *
+ * @author Alexander vallejo
+ */
+public class Cv_ejercicioo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("versión 1");
+        
+    }
+    
+}
